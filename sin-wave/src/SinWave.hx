0e3d267c6927aedd793c6c4db144d3d6c38651a3
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-09-24 21:11:51 -0400

    Sin Wave example

--- sin-wave/src/SinWave.hx
+++ sin-wave/src/SinWave.hx
@@ -2,6 +2,19 @@ class SinWave {
 
     public function new() {
         trace('Sin Wave');
+
+        var n = 0;
+        var volume = 0.3;
+        var frequency = 425;
+
+        AudioPlayer
+            .create()
+            .useGenerator((out, sampleRate) -> for (i in 0...out.length >> 1) {
+                var sin = MathUtils.sin( 2 * MathUtils.PI * n * frequency / sampleRate ) * volume;
+                out.set(i*2, sin);
+                out.set(i*2 + 1, sin);
+                n++;
+            });
     }
 
     static public function main() {
