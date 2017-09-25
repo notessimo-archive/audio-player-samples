c6aa33f059ea6592e42fc147af5c4c62de68c6a9
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-09-25 08:45:07 -0400

    Distinction between left and right

--- sin-wave/src/SinWave.hx
+++ sin-wave/src/SinWave.hx
@@ -16,8 +16,8 @@ class SinWave {
             .create()
             .useGenerator((out, sampleRate) -> for( i in 0...out.length >> 1 ) {
                 var sin = MathUtils.sin(2 * MathUtils.PI * n * frequency / sampleRate) * volume;
-                out.set(i*2, sin);
-                out.set(i*2 + 1, sin);
+                out.set(i*2, sin); // Left
+                out.set(i*2 + 1, sin); // Right
                 n++;
             })
             .play();
